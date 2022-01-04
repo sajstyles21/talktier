ed94455fbc3397f45036dba815dd5ee35927a126
Suraj <surajsharma@Surajs-MacBook-Air.local>
2022-01-04 22:44:11 +0530

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class HomeController extends Controller
